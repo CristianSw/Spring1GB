d0004007986d61d980804ec3a73a898cb383c1ed
CristianSw <cristian.sw@protonmail.com>
2022-09-18 23:39:19 +0300

    Homework 7 added username filter, remain to do email filter.

--- homework4-spring-boot/src/main/java/com/cdolinta/controller/UserController.java
+++ homework4-spring-boot/src/main/java/com/cdolinta/controller/UserController.java
@@ -7,12 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -23,8 +21,13 @@ public class UserController {
     private final UserRepository userRepository;
 
     @GetMapping
-    public String listPage(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+    public String listPage(@RequestParam Optional<String> usernameFilter, Model model) {
+        if (usernameFilter.isEmpty() || usernameFilter.get().isBlank()) {
+            model.addAttribute("users", userRepository.findAll());
+        } else {
+            model.addAttribute("users", userRepository.findAllByUsernameLike
+                    ("%" + usernameFilter.get() + "%"));
+        }
         return "user";
     }
 
@@ -42,7 +45,7 @@ public class UserController {
 
     @GetMapping("/delete/{id}")
     public String deleteUserById(@PathVariable long id) {
-        userRepository.delete(id);
+        userRepository.deleteById(id);
         return "redirect:/user";
     }
 
